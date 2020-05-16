bd63309b70422dab8c75312dcc857a94be283b2d
ranshao2017 <zhangqh2012@163.com>
2020-05-17 02:23:01 +0800

    企业身份核验

--- api/user.js
+++ api/user.js
@@ -13,10 +13,36 @@ export const sendVerificationCode = (sessionid, phone) => {
   })
 }
 
+/**
+ * 申请历史查询
+ */
 export const getApplyHis = (sessionid, phone) => {
   return http.get('getApplyHis.do', {
 	params: {
 		sessionid
 	}
+  })
+}
+
+/**
+ * 根据社信码查询企业工商登记信息
+ */
+export const getCompanyInfoByCode = (sessionid, companycode) => {
+  return http.post('getCompanyInfoByCode.do', {
+	sessionid,
+	companycode
+  })
+}
+
+/**
+ * 根据社信码和法人信息核验企业身份
+ */
+export const checkCompany = (sessionid, companycode, companyname, legalname, legalsfzhm) => {
+  return http.post('checkCompany.do', {
+	sessionid,
+	companycode,
+	companyname,
+	legalname,
+	legalsfzhm
   })
 }
--- pages/userData/checktwo.vue
+++ pages/userData/checktwo.vue
@@ -2,28 +2,45 @@
   <view>
 	  <view class="row">
 	  	<text class="left">企业法人身份证号码</text>
-	  	<input class="right" type="text" placeholder="请输入企业法人身份证号码" placeholder-class="placeholder"/>
+	  	<input class="right" type="text" v-model="legalsfzhm" placeholder="请输入企业法人身份证号码" placeholder-class="placeholder"/>
 	  </view>
 	  <u-button class="btntel" @click="applyImmediate">确认</u-button>
   </view>
 </template>
 
 <script>
+import { checkCompany } from '@/api/user.js';
 export default {
   data() {
     return {
       customStyle: {
         fontSize: '12px'
-      }
+      },
+	  companycode: '',
+	  companyname:'',
+	  legalname:'',
+	  legalsfzhm: '',
+	  disable: false
     };
   },
   methods: {
-    applyImmediate() {
-      console.log('立即申请');
+    async applyImmediate() {
+		if(!this.legalsfzhm){
+			uni.showToast({ title: '请输入企业法人身份证号码', icon: 'none' });
+			return;
+		}
+		const res = await checkCompany('2d34d7b18cf62de6547adde3ea992ae2', this.companycode, this.companyname, this.legalname, this.legalsfzhm);
+		if(0 == res.respcode){
+			uni.showToast({ title: '核验成功', icon: 'none' });
+		}else{
+			uni.showToast({ title: res.respinfo, icon: 'none' });
+		}
     }
   },
   onLoad(option) {
-    console.log(option);
+	  this.companycode = option.companycode;
+	  this.companyname = option.companyname;
+	  this.legalname = option.legalname;
   }
 };
 </script>
