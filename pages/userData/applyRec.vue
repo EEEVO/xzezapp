8eb748d5cf8f9de084124dce317b998be7dcb502
zhangqinghe <516567166@qq.com>
2020-05-16 19:28:23 +0800

    申请记录

--- pages/userData/applyRec.vue
+++ pages/userData/applyRec.vue
@@ -1,12 +1,23 @@
 <template>
   <view class="recontent">
-	  <view class="list" v-for="(list, list_i) in severList" :key="list_i">
-	    <view class="li" v-for="(li, li_i) in list" @tap="toPage(list_i, li_i)" :class="{ noborder: li_i == list.length - 1 }" hover-class="hover" :key="li.name">
-	      <view class="icon"><image :src="'../../static/personalCenter/sever/' + li.icon"></image></view>
-	      <view class="text">{{ li.name }}</view>
-	      <image class="to" src="../../static/personalCenter/to.png"></image>
-	    </view>
-	  </view>
+	<view class="row" v-for="(li, li_i) in severList">
+		<view class="signrow">
+			<text class="left">产品名称</text>
+			<text class="right">{{ li.name }}</text>
+		</view>
+		<view class="signrow">
+			<text class="left">申请日期</text>
+			<text class="right">{{ li.applydate }}</text>
+		</view>
+		<view class="signrow">
+			<text class="left">当前状态</text>
+			<text class="right">{{ li.status }}</text>
+		</view>
+		<view class="signrow">
+			<text class="left">需求描述</text>
+			<text class="right">{{ li.remark }}</text>
+		</view>
+	</view>
   </view>
 </template>
 
@@ -16,8 +27,8 @@ export default {
     return {
       items: ['全部设备', '个人设备'],
       current: 0,
-	  severList: [[{ name: '申请记录', icon: 'applyRec.png', link: '../userData/applyRec' },
-	  				   { name: '企业身份核验', icon: 'check.png', link: '../userData/check' }]]
+	  severList: [{ name: '税e贷', applydate: '2020-05-08', status: '待审核', remark: '需求描述需求描述' },
+	  				   { name: '商超e贷', applydate: '2020-05-08', status: '审核通过', remark: '需求描述需求描述' }]
     };
   },
   methods: {}
@@ -27,9 +38,31 @@ export default {
 <style scoped lang="less">
 	.recontent{
 		display: flex;
+		flex-wrap:wrap;
+		justify-content:center;
 		align-items: center;
-		position: relative;
-		background: #fff;
+		background: #f1f1f1;
 		font-size: 30upx;
+		
+		.row{
+			width: 98%;
+			margin-top: 20upx;
+			background: #fff;
+			padding: 20upx;
+			
+			.signrow{
+				display: flex;
+				padding:0 30upx;
+				height: 60upx;
+				
+				.left{
+					text-align: right;
+				}
+				.right{
+					padding-left: 50upx;
+					display: flex;
+				}
+			}
+		}
 	}
 </style>
